39eb0f496a85851c0de016d11ef27e8d6281c1ea
AnetoliyBoltovski <tolikboltovskij11@gmail.com>
2024-12-08 17:37:11 +0200

    update

--- game.py
+++ game.py
@@ -2,3 +2,14 @@ def print_board(board):
     for row in board:
         print(' | '.join(row))
         print('-' * 9)
+
+def check_winner(board,player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+    for col in range(3):
+        if all(row[col] == playr for row in board):
+            return True
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False 
